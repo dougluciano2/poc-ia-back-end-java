5bbf85319d1fb22de33deeeb425e25f345754421
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-09-01 23:10:26 -0300

    Initial commit

---
+++ src/main/java/io/dougluciano/poc/api_notas/API/notas/Fiscais/ApiNotasFiscaisApplication.java
@@ -0,0 +1,13 @@
+package io.dougluciano.poc.api_notas.API.notas.Fiscais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotasFiscaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNotasFiscaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dougluciano/poc/api_notas/API/notas/Fiscais/ApiNotasFiscaisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dougluciano.poc.api_notas.API.notas.Fiscais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotasFiscaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
